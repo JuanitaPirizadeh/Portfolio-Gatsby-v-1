6d2535b62311a71c5e565def1cbf99c211c34357
JuanitaPirizadeh <jupirizadeh@gmail.com>
2019-09-24 17:46:18 +0200

    new colors

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -6,11 +6,11 @@ import services from '../../constants/services'
 const Services = () => {
     return (
        <section className={styles.services}>
-           <Title title="my" subtitle="skills" />
+           <Title title="My" subtitle="Skills" />
            <div className={styles.center}>
         {
             services.map((item,index)=>{
-                return <article key={index} className={styles.services}>
+                return <article key={index} className={styles.service}>
                     <span>{item.icon}</span>
                     <h4>{item.title}</h4>
                     <p>{item.text}</p>
